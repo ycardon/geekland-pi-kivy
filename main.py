232bf9a5596b546609b477360cfdd5515dccffad
Yann <ycardon@mbp-yann.home>
2015-09-24 19:52:33 +0200

    added ip address in main panel

--- main.py
+++ main.py
@@ -51,6 +51,9 @@ while True:
 		print('GeeklandRemote is waiting for network')
 		time.sleep(1)
 
+# --- Main ---
+class MainPanel(BoxLayout):
+	ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
 
 # --- Weather ---
 class WeatherPanel(BoxLayout):
