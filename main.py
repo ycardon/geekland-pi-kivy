5c2a741f5676f8b54c1896771b4e1164866d6fa6
Yann <ycardon@mbp-yann.home>
2015-10-13 12:58:04 +0200

    more TODO

--- main.py
+++ main.py
@@ -60,6 +60,8 @@ while True:
 
 # --- Main ---
 class MainPanel(BoxLayout):
+
+	# get local ip
 	ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
 
 # --- Weather ---
